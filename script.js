e0c83096983a49048b4e2e15ead86189abf584fd
khatiachub <101997849+khatiachub@users.noreply.github.com>
2024-01-24 20:13:11 +0400

    added slider

--- script.js
+++ script.js
@@ -20,7 +20,115 @@ burger.addEventListener('click',function(){
     burger.classList.toggle('active');
     burger.classList.toggle('color')
 })
-// document.addEventListener('click',function(){
-//     nav.classList.remove('d-block');
 
-// })
+
+//slider
+   
+        const slider = document.querySelector('.slider');
+        const prevButton=document.querySelector('.prev')
+        const nextButton=document.querySelector('.next')
+        const slides = document.querySelectorAll('.slide');
+        let dots = document.querySelectorAll(".dot");
+
+        let slideIndex = 0;
+
+     
+       // mobile rezoluciaze
+      const slideImage=()=>{
+        slides.forEach(
+          (slide,index)=>{
+            slide.style.left = `${index * 100}%`;
+          }
+        );
+        slides.forEach(
+          (slide)=>{
+            slide.style.transform = `translateX(-${slideIndex * 100}%)`;
+          }
+        );
+      }
+
+      //desktop rezoluciaze
+      function showSlidesDesktop() {
+        if(slideIndex>slides.length-1){
+          slideIndex=0
+        }
+        else if(slideIndex<0){
+          slideIndex=slides.length-1
+        }
+      
+        slides.forEach(
+          (slide,i)=>{
+            slide.style.display =(i === slideIndex) ? "flex" : "none";
+          }
+        );
+    }
+    function handleResize() {
+      if (window.innerWidth < 1024) {
+          slideImage();
+          slides.forEach(
+            (slide,i)=>{
+              slide.style.display ="flex"
+            }
+          );
+      } else {
+          showSlidesDesktop();
+      }
+  }    
+
+      //ukan
+      function prevSlide(){
+          showSlidesDesktop()
+          slideIndex--
+      }
+
+      //win
+      function nextSlide(){
+          showSlidesDesktop()
+          slideIndex++        
+      }
+
+      handleResize();
+      window.addEventListener('resize', handleResize);
+      prevButton.addEventListener('click',prevSlide)
+      nextButton.addEventListener('click',nextSlide)
+
+      // setInterval( () => {
+      //   setSlideTimer()
+      //   }, 1000)
+
+      function setSlideTimer(){
+        if(window.innerWidth<1024){
+          if (slideIndex >= slides.length ) {         
+            slideIndex = 0;
+            slideImage();
+            // slides.forEach(
+            //   (slide)=>{
+            //     slide.style.transition = 'none';  
+            //     void slide.offsetWidth;
+            //     slide.style.transition = '';
+            // }
+            // );
+        } else {
+            slideImage()
+            slideIndex++
+        }
+        }else{
+          showSlidesDesktop()
+          slideIndex++
+        }
+      }
+
+        dots.forEach((dot, index) => {
+            dot.addEventListener('click', function () {
+              slideIndex = index;
+              if(window.innerWidth<1024){
+                  slideImage()
+              }else{
+                showSlidesDesktop()
+              }
+            });
+        });
+
+        
+      
+     
